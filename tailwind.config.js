863e0303811c8ec06d3d224fb3701ab63e51974b
Sebastian Winther <sebastianwinther@gmail.com>
2019-12-01 22:32:48 +0100

    font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   theme: {
+    fontFamily: {
+      sans:
+        '"Kontrapunkt Miki", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'
+    },
     extend: {
       boxShadow: theme => ({
         outline: `0 0 0 2px ${theme("colors.pink.600")}`
