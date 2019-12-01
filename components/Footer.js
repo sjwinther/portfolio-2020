97842e77b5720ac0d392728fb364f008ee9a9d10
Sebastian Winther <sebastianwinther@gmail.com>
2019-12-01 18:42:16 +0100

    loading spinner in footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,36 @@
-export default () => (
-  <footer className="px-4">
-    <div className="max-w-5xl border-t-2 border-gray-800 py-16 mx-auto">
-      <p className="font-medium text-xl text-center mb-4">
-        I'm available for work. Write me ✉️, call me ☎️, or leave your email:
-      </p>
-      <div className="max-w-md flex items-center mx-auto">
-        <input
-          type="email"
-          placeholder="tim@apple.com"
-          className="input flex-auto m-2"
-        />
-        <button className="btn btn-primary m-2">Send it along</button>
+import { useState } from "react";
+
+import LoadingSpinner from "../components/LoadingSpinner";
+
+export default () => {
+  const [loading, setLoading] = useState(false);
+  return (
+    <footer className="px-4">
+      <div className="max-w-5xl border-t-2 border-gray-800 py-8 mx-auto">
+        <p className="font-medium text-xl text-center mb-4">
+          I'm available for work. Write me ✉️, call me ☎️, or leave your email:
+        </p>
+        <div className="max-w-md flex items-center mx-auto">
+          <input
+            type="email"
+            placeholder="tim@apple.com"
+            className="input flex-auto m-2"
+          />
+          <button
+            onClick={() => setLoading(true)}
+            className="btn btn-primary relative m-2"
+          >
+            <span className={loading ? "text-transparent" : ""}>
+              Send it along
+            </span>
+            {loading && (
+              <div className="absolute inset-0 flex items-center justify-center">
+                <LoadingSpinner />
+              </div>
+            )}
+          </button>
+        </div>
       </div>
-    </div>
-  </footer>
-);
+    </footer>
+  );
+};
