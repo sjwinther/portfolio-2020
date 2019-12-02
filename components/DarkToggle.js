f147ffd93e2a8b253c35e2c85544cb18104ae868
Sebastian Winther <sebastianwinther@gmail.com>
2019-12-02 20:59:48 +0100

    works with local storage

--- components/DarkToggle.js
+++ components/DarkToggle.js
@@ -1,27 +1,53 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
-export default () => {
+export default class ToggleWrapper extends React.Component {
+  state = {
+    localDark: undefined,
+    prefersDark: undefined
+  };
+  componentDidMount() {
+    this.setState({
+      localDark: JSON.parse(localStorage.getItem("dark")),
+      prefersDark:
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+    });
+  }
+  render() {
+    const { localDark, prefersDark } = this.state;
+    if (localDark !== undefined && prefersDark !== undefined)
+      return <Toggle localDark={localDark} prefersDark={prefersDark} />;
+    return null;
+  }
+}
+
+const Toggle = ({ localDark, prefersDark }) => {
   const [dark, setDark] = useState(
-    typeof window !== "undefined" &&
-      window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches
+    localDark === null ? prefersDark : localDark
   );
   useEffect(() => {
-    dark && document.documentElement.classList.add("mode-dark");
-    !dark && document.documentElement.classList.remove("mode-dark");
+    if (dark) {
+      document.documentElement.classList.add("mode-dark");
+      localStorage.setItem("dark", true);
+    }
+    if (!dark) {
+      document.documentElement.classList.remove("mode-dark");
+      localStorage.setItem("dark", false);
+    }
   }, [dark]);
   return (
     <button
-      className="fixed right-0 bottom-0 z-10 w-10 h-6 flex items-center bg-gray-900 dark:bg-gray-300 rounded-full mr-4 mb-4"
+      className="hidden lg:flex items-center fixed right-0 bottom-0 z-10 w-10 h-6 bg-gray-800 dark:bg-gray-300 rounded-full mr-4 mb-4"
       onClick={() => setDark(!dark)}
     >
       <motion.div
+        initial={dark ? { left: 20 } : { left: 4 }}
         animate={dark ? { left: 20 } : { left: 4 }}
         transition={{ type: "tween" }}
         className="absolute w-4 h-4"
       >
-        <div className="w-full h-full bg-gray-300 dark:bg-gray-900 rounded-full transition" />
+        <div className="w-full h-full bg-white dark:bg-black rounded-full transition" />
       </motion.div>
     </button>
   );
