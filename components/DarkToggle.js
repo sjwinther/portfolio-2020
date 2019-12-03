2d93925af899a3f988f437f3b4c754e731dee39b
Sebastian Winther <sebastianwinther@gmail.com>
2019-12-03 21:07:25 +0100

    bigger toggle

--- components/DarkToggle.js
+++ components/DarkToggle.js
@@ -38,16 +38,16 @@ const Toggle = ({ localDark, prefersDark }) => {
   }, [dark]);
   return (
     <button
-      className="hidden lg:flex items-center fixed right-0 bottom-0 z-10 w-10 h-6 bg-gray-800 dark:bg-gray-300 rounded-full mr-4 mb-4"
+      className="hidden lg:flex items-center fixed right-0 bottom-0 z-10 w-12 h-8 bg-gray-800 dark:bg-gray-300 rounded-full mr-4 mb-4"
       onClick={() => setDark(!dark)}
     >
       <motion.div
-        initial={dark ? { left: 20 } : { left: 4 }}
-        animate={dark ? { left: 20 } : { left: 4 }}
+        initial={dark ? { left: 22 } : { left: 6 }}
+        animate={dark ? { left: 22 } : { left: 6 }}
         transition={{ type: "tween" }}
-        className="absolute w-4 h-4"
+        className="absolute w-5 h-5"
       >
-        <div className="w-full h-full bg-white dark:bg-black rounded-full transition" />
+        <div className="w-full h-full bg-white dark:bg-black rounded-full shadow transition" />
       </motion.div>
     </button>
   );
