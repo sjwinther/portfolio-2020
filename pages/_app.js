66b0c7343399c46220e6afab2ec67cbd93df4908
Sebastian Winther <sebastianwinther@gmail.com>
2021-07-16 16:13:13 +0200

    Fix postcss

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import App from 'next/app'
 import splitbee from '@splitbee/web'
 
@@ -7,31 +8,27 @@ import Store from '../store'
 import Head from '../components/Head'
 import Wrapper from '../components/Wrapper'
 
-class MyApp extends App {
-  state = {}
-  componentDidMount() {
-    this.setState({ mounted: true })
+export default function MyApp({ Component, pageProps }) {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
     if (process.env.NODE_ENV === 'production') {
       splitbee.init({
         token: 'IUOZY01RM0F4',
         disableCookie: true,
       })
     }
-  }
-  render() {
-    const { Component, pageProps } = this.props
-    const { mounted } = this.state
-    return (
-      <Store>
-        <Head />
-        {mounted && (
-          <Wrapper>
-            <Component {...pageProps} />
-          </Wrapper>
-        )}
-      </Store>
-    )
-  }
-}
+  }, [])
 
-export default MyApp
+  return (
+    <Store>
+      <Head />
+      {mounted && (
+        <Wrapper>
+          <Component {...pageProps} />
+        </Wrapper>
+      )}
+    </Store>
+  )
+}
--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,19 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['./components/**/*.js', './css/**/*.css', './pages/**/*.js'],
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-  whitelist: ['mode-dark'],
-})
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    purgecss: process.env.NODE_ENV === 'production' ? {} : false,
+    '@fullhuman/postcss-purgecss':
+      process.env.NODE_ENV === 'production'
+        ? {
+            content: [
+              './components/**/*.js',
+              './css/**/*.css',
+              './pages/**/*.js',
+            ],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+            whitelist: ['mode-dark'],
+          }
+        : false,
     cssnano: {},
   },
 }
